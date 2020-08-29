1341bc11561baccf28fd9338fe83446dbc1b9098
Akshay S <akshay@acism.com>
2020-08-29 13:11:52 +0530

    Code is complete. Working on test cases

--- src/currencyToText/currencyToText.java
+++ src/currencyToText/currencyToText.java
@@ -5,12 +5,13 @@ import java.io.*;
 public class currencyToText
 {
 	private static final String EMPTY = "";
-
+	// When number is 1 to 19
 	private static final String[] X ={ EMPTY, "One", "Two", "Three", "Four", "Five", "Six",
 	"Seven ", "Eight ", "Nine", "Ten", "Eleven","Twelve",
 	"Thirteen ", "Fourteen ", "Fifteen ", "Sixteen ",
 	"Seventeen", "Eighteen ", "Nineteen "};
-
+	
+	// For number 20 and above it for a prefix
 	private static final String[] Y = {EMPTY, EMPTY, "Twenty ", "Thirty ", "Forty ", "Fifty ",
 	"Sixty ", "Seventy ", "Eighty ", "Ninety " };
 
@@ -33,48 +34,63 @@ public class currencyToText
 
 	public static String convert(int n)
 	{
+		if(n == 0)
+			return("Zero");
+		
+		else if( n < 0 )
+			return("Negative Numbers not allowed");
+		
+		/*  because the limit is 999999.99, 
+			We can simply remove this  else if section below to extend the limit */
+		else if ( n > 999999.99 )  
+			return("Reached Max Value");
+		
+		
 		StringBuilder res = new StringBuilder();
-
+	
 		res.append(convertToDigit(((n / 100000) % 100), " Lakh, "));
-
+	
 		res.append(convertToDigit(((n / 1000) % 100), " Thousand "));
-
+	
 		res.append(convertToDigit(((n / 100) % 10), " Hundred "));
-
+	
 		if ((n > 100) && (n % 100 != 0))
 		{
 			res.append("and ");
 		}
-
+	
 		res.append(convertToDigit((n % 100), ""));
-		return res.toString();
+		String temp = res.toString();
+		return temp;
+		
 	}
 
 		public static void main(String[] args)
 		{
-			Double doubleValue;
-			int n;
+			Double doubleValue; // value to get from user
+			int n; // to save the non fractional value from the given input
+			
 			System.out.println("\nEnter a number : ");
 			Scanner sc = new Scanner(System.in);
 			doubleValue = sc.nextDouble();
-			if(doubleValue == 0)
-			{
-				System.out.println("Rs. Zero Only");
-				System.exit(1);
-			}
-			else if ( doubleValue > 999999.99 )
-			{
-				System.out.println("Reached Max Value");
-				System.exit(1);
-			}
+			
+			//When number is Zero we do not need the whole program to run in the below statement
+			
 			String numberStr = Double.toString(doubleValue);
 			String fractionalStr = numberStr.substring(numberStr.indexOf('.')+1);
 			int fractional = Integer.valueOf(fractionalStr);
 			n = (int)Math.round(doubleValue);
-
+			
+			/* we use the Math.round method to get the number before the fraction, 
+			as it will round up the integer if fraction is above 50 We use the bellow statement 
+			 to get the original number */
+			
 			if (fractional > 50)
 				n = n - 1;
-
+			
+			//when there is no fraction used, we don't need fractional/100 See the below statement
+			
+			
 			if (fractional == 0)
 			{
 				System.out.println(" Rs.  " + convert(n) + " Only" ) ;
--- src/currencyToText/currencyToTextTest.java
+++ src/currencyToText/currencyToTextTest.java
@@ -4,21 +4,31 @@ import static org.junit.Assert.*;
 import currencyToText.currencyToText;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Test;
 
 public class currencyToTextTest {
 
-	 @Test
+	@Test
 	    public void Integer() throws Exception {
 	       String result = currencyToText.convert(12345);
 	       result = "Rs. " + result + " Only" ;
 	       assertThat(result, is("Rs. Twelve Thousand Three Hundred and Forty Five Only"));
-	 }
+	 } 
 	 @Test
 	    public void Zero() throws Exception {
-	       //if (doubleValue == 0)
-	    	 //  assertThat(result, is("Rs. Twelve Thousand Three Hundred and Forty Five Only"));
+		 	String resultForZero = currencyToText.convert(0);
+		 	resultForZero = "Rs. " + resultForZero + " Only" ;
+	    	assertThat(resultForZero, is("Rs. Zero Only"));
 	 }
+	 
+	 @Test
+	 public void MinusValue() throws Exception {
+		 	String resultForZero = currencyToText.convert(-12);
+		 	resultForZero = "Rs. " + resultForZero + " Only" ;
+	    	assertThat(resultForZero, is("Rs. Negative Numbers not allowed Only"));
+	 }
+	 
+	/* Note :  As the currencyToText.convert() takes integer only 
+	 * 	there is no need to check string inputs as it will give compilation errors */
 }
